c2a5d1972fe61dc24e9aae5c4476a06930e8014f
yoshikawa keita <yoshikawakei@yoshikawakeinoMacBook-Pro.local>
2023-06-18 00:39:41 +0900

    health check func

--- pb/service_economic_school.pb.gw.go
+++ pb/service_economic_school.pb.gw.go
@@ -32,6 +32,24 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+func request_EconomicSchool_HealthCheck_0(ctx context.Context, marshaler runtime.Marshaler, client EconomicSchoolClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq emptypb.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.HealthCheck(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_EconomicSchool_HealthCheck_0(ctx context.Context, marshaler runtime.Marshaler, server EconomicSchoolServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq emptypb.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.HealthCheck(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_EconomicSchool_CreateUser_0(ctx context.Context, marshaler runtime.Marshaler, client EconomicSchoolClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateUserRequest
 	var metadata runtime.ServerMetadata
@@ -714,6 +732,31 @@ func local_request_EconomicSchool_GetExamCountByUniversity_0(ctx context.Context
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterEconomicSchoolHandlerFromEndpoint instead.
 func RegisterEconomicSchoolHandlerServer(ctx context.Context, mux *runtime.ServeMux, server EconomicSchoolServer) error {
 
+	mux.Handle("GET", pattern_EconomicSchool_HealthCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/pb.EconomicSchool/HealthCheck", runtime.WithHTTPPathPattern("/health"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_EconomicSchool_HealthCheck_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_EconomicSchool_HealthCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_EconomicSchool_CreateUser_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1155,6 +1198,28 @@ func RegisterEconomicSchoolHandler(ctx context.Context, mux *runtime.ServeMux, c
 // "EconomicSchoolClient" to call the correct interceptors.
 func RegisterEconomicSchoolHandlerClient(ctx context.Context, mux *runtime.ServeMux, client EconomicSchoolClient) error {
 
+	mux.Handle("GET", pattern_EconomicSchool_HealthCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/pb.EconomicSchool/HealthCheck", runtime.WithHTTPPathPattern("/health"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_EconomicSchool_HealthCheck_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_EconomicSchool_HealthCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_EconomicSchool_CreateUser_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1511,6 +1576,8 @@ func RegisterEconomicSchoolHandlerClient(ctx context.Context, mux *runtime.Serve
 }
 
 var (
+	pattern_EconomicSchool_HealthCheck_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"health"}, ""))
+
 	pattern_EconomicSchool_CreateUser_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "create_user"}, ""))
 
 	pattern_EconomicSchool_UpdateUser_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "update_user"}, ""))
@@ -1545,6 +1612,8 @@ var (
 )
 
 var (
+	forward_EconomicSchool_HealthCheck_0 = runtime.ForwardResponseMessage
+
 	forward_EconomicSchool_CreateUser_0 = runtime.ForwardResponseMessage
 
 	forward_EconomicSchool_UpdateUser_0 = runtime.ForwardResponseMessage
