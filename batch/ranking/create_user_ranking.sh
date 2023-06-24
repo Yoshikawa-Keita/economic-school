264ed4db0dfc2ca128813d18e800d4a78a5cdb08
yoshikawa keita <yoshikawakei@yoshikawakeinoMacBook-Pro.local>
2023-06-24 18:33:49 +0900

    add rank column

--- batch/ranking/create_user_ranking.sh
+++ batch/ranking/create_user_ranking.sh
@@ -19,12 +19,11 @@ TRUNCATE global_ranking
 
 # Create the overall ranking
 psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v ON_ERROR_STOP=1 -c "
-INSERT INTO global_ranking (username, num_completed_exams, ranking_date)
-SELECT username, COUNT(*), '$DATE_TO'
+INSERT INTO global_ranking (username, num_completed_exams, ranking, ranking_date)
+SELECT username, COUNT(*), RANK() OVER (ORDER BY COUNT(*) DESC), '$DATE_TO'
 FROM user_exams
 WHERE is_completed = true
 GROUP BY username
-ORDER BY COUNT(*) DESC
 "
 
 # Truncate the existing university-specific ranking
@@ -34,12 +33,11 @@ TRUNCATE university_ranking
 
 # Create the university-specific ranking
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -v ON_ERROR_STOP=1 -c "
-INSERT INTO university_ranking (username, university, num_completed_exams, ranking_date)
-SELECT username, university, COUNT(*), '$DATE_TO'
+INSERT INTO university_ranking (username, university, num_completed_exams, ranking, ranking_date)
+SELECT username, university, COUNT(*), RANK() OVER (PARTITION BY university ORDER BY COUNT(*) DESC), '$DATE_TO'
 FROM user_exams
 WHERE is_completed = true
 GROUP BY username, university
-ORDER BY COUNT(*) DESC
 "
 
 # Truncate the existing weekly overall ranking
@@ -49,12 +47,11 @@ TRUNCATE weekly_global_ranking
 
 # Create the weekly overall ranking
 psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v ON_ERROR_STOP=1 -c "
-INSERT INTO weekly_global_ranking (username, completed_exams_count, ranking_date)
-SELECT username, COUNT(*), '$DATE_TO'
+INSERT INTO weekly_global_ranking (username, completed_exams_count, ranking, ranking_date)
+SELECT username, COUNT(*), RANK() OVER (ORDER BY COUNT(*) DESC), '$DATE_TO'
 FROM user_exams
 WHERE is_completed = true AND completed_at BETWEEN TO_TIMESTAMP($DATE_FROM, 'YYYYMMDD') AND (TO_TIMESTAMP($DATE_TO, 'YYYYMMDD') + INTERVAL '1 day')
 GROUP BY username
-ORDER BY COUNT(*) DESC
 "
 
 # Truncate the existing weekly university-specific ranking
@@ -64,10 +61,9 @@ TRUNCATE weekly_university_ranking
 
 # Create the weekly university-specific ranking
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -v ON_ERROR_STOP=1 -c "
-INSERT INTO weekly_university_ranking (username, university, completed_exams_count, ranking_date)
-SELECT username, university, COUNT(*), '$DATE_TO'
+INSERT INTO weekly_university_ranking (username, university, completed_exams_count, ranking, ranking_date)
+SELECT username, university, COUNT(*), RANK() OVER (PARTITION BY university ORDER BY COUNT(*) DESC), '$DATE_TO'
 FROM user_exams
 WHERE is_completed = true AND completed_at BETWEEN TO_TIMESTAMP($DATE_FROM, 'YYYYMMDD') AND (TO_TIMESTAMP($DATE_TO, 'YYYYMMDD') + INTERVAL '1 day')
 GROUP BY username, university
-ORDER BY COUNT(*) DESC
 "
